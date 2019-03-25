62e4365f1dc4336693673562cd53665e942f33e4
Ilario Pierbattista <ilario.pierbattista@facile.it>
2019-03-25 09:07:45 +0100

    first option

---
+++ exercises/src/test/scala/fpinscala/errorhandling/OptionTest.scala
@@ -0,0 +1,11 @@
+package fpinscala.errorhandling
+
+import utest.{TestSuite, Tests, assert}
+
+object OptionTest extends TestSuite {
+  val tests = Tests {
+    'hasSubsequence - {
+      assert()
+    }
+  }
+}
