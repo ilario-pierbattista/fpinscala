1c737e32e5b0fc209f451991dad7232bf5044881
Ilario Pierbattista <ilario.pierbattista@facile.it>
2019-04-02 23:56:08 +0200

    too lazy to commit

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,15 +1,22 @@
 package fpinscala.laziness
 
-import Stream._
+import scala.annotation.tailrec
+
 trait Stream[+A] {
+
+  def toList: List[A] =
+    this.fold(
+      Nil: List[A],
+      s => s.h() :: s.t().toList
+    )
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,11 +24,42 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = ???
 
-  def drop(n: Int): Stream[A] = ???
+  def fold[B, F[_]](
+                     whenEmpty: => F[B],
+                     whenCons: Cons[A] => F[B]
+                   ): F[B] =
+    this match {
+      case Cons(h, t) => whenCons(Cons(h, t))
+      case Empty => whenEmpty
+    }
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def take(n: Int): Stream[A] =
+    this.fold(
+      Empty,
+      cons =>
+        if (n > 0) Cons(cons.h, () => cons.t().take(n - 1))
+        else Empty
+    )
+
+  def drop(n: Int): Stream[A] =
+    this.fold(
+      Empty,
+      {
+        case Cons(h, t) =>
+          if (n > 0) t().drop(n - 1)
+          else Cons(h, t)
+      }
+    )
+
+  def takeWhile(p: A => Boolean): Stream[A] =
+    this.fold(
+      Empty,
+      { case Cons(h, t) =>
+        if (p(h())) Cons(h, () => t().takeWhile(p))
+        else Empty
+      }
+    )
 
   def forAll(p: A => Boolean): Boolean = ???
 
@@ -32,7 +70,9 @@ trait Stream[+A] {
 
   def startsWith[B](s: Stream[B]): Boolean = ???
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -45,10 +85,11 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
+
   def from(n: Int): Stream[Int] = ???
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
---
+++ exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
@@ -0,0 +1,32 @@
+package fpinscala.laziness
+
+import java.util.function.Predicate
+
+import utest._
+
+import scala.{Option => _, Some => _, Stream => _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+
+
+object StreamTest extends TestSuite {
+  val tests = Tests {
+    'toList - {
+      assert(List() == Stream().toList)
+      assert(List(1, 2, 3) == Stream(1, 2, 3).toList)
+    }
+
+    'take - {
+      assert(List() == Stream().take(2).toList)
+      assert(List(1, 2) == Stream(1, 2, 3, 4).take(2).toList)
+    }
+
+    'drop - {
+      assert(List() == Stream().drop(2).toList)
+      assert(List(3, 4) == Stream(1, 2, 3, 4).drop(2).toList)
+    }
+
+    'takeWhile - {
+      assert(List() == (Stream(): Stream[Int]).takeWhile(_ <= 2).toList)
+      assert(List(1, 2) == Stream(1, 2, 3, 4).takeWhile(_ <= 2).toList)
+    }
+  }
+}
