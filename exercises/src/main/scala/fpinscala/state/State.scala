c3006e32726abea31938349e7a70cbd22db4b85b
Ilario Pierbattista <ilario.pierbattista@facile.it>
2019-04-26 08:11:09 +0200

    state

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -6,6 +6,7 @@ trait RNG {
 }
 
 object RNG {
+
   // NB - this was called SimpleRNG in the book text
 
   case class Simple(seed: Long) extends RNG {
@@ -24,47 +25,84 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) =
+    rng.nextInt match {
+      case (Int.MinValue, s) => (Int.MaxValue, s)
+      case (v, s) => (Math.abs(v), s)
+    }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val r = RNG.nonNegativeInt(rng)
+    (r._1.toDouble / Int.MaxValue.toDouble, r._2)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val n = rng.nextInt
+    val m = RNG.double(n._2)
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+    ((n._1, m._1), m._2)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val n = RNG.double(rng)
+    val m = n._2.nextInt
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+    ((n._1, m._1), m._2)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val n = RNG.double(rng)
+    val m = RNG.double(n._2)
+    val l = RNG.double(m._2)
+
+    ((n._1, m._1, l._1), l._2)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0)
+      (Nil, rng)
+    else {
+      val (x, nextRng) = rng.nextInt
+      val (subList, finalRng) = ints(count - 1)(nextRng);
+
+      (x :: subList, finalRng)
+    }
+  }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
-case class State[S,+A](run: S => (A, S)) {
+case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     ???
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     ???
+
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     ???
 }
 
 sealed trait Input
+
 case object Coin extends Input
+
 case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
--- exercises/src/test/scala/fpinscala/state/StateTest.scala
+++ exercises/src/test/scala/fpinscala/state/StateTest.scala
@@ -2,11 +2,23 @@ package fpinscala.state
 
 import utest._
 
-
 object StateTest extends TestSuite {
   val tests = Tests {
     'base - {
-      
+      val (a, rnga) = RNG.Simple(42).nextInt
+      val (b, rngb) = rnga.nextInt
+      val (n, _) = rngb.nextInt
+      assert(16159453 == a)
+      assert(-1281479697 == b)
+      assert(-340305902 == n)
+    }
+
+    'ints - {
+      assertMatch(
+        RNG.ints(3)(RNG.Simple(42))
+      ) {
+        case (List(16159453, -1281479697, -340305902), _) =>
+      }
     }
   }
 }
